c016c46c3ced87a0bf9e5449530fa825594539d4
Ana Carolina Minadakis <ana.minadakis@zup.com.br>
2021-06-29 18:20:36 -0300

    050- login via senha

---
+++ src/main/java/br/com/zup/anaminadakis/proposta/seguranca/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.zup.anaminadakis.proposta.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().httpStrictTransportSecurity().disable();
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests.antMatchers(HttpMethod.GET, "/**")
+                        .hasAuthority("SCOPE_meu-primeiro-escopo")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    }
+}
